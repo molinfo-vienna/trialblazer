ea28f513720fe185a70d57b09f96c52e8d9f7cef
Huanni Zhang <huanni.zhang@univie.ac.at>
2025-05-07 17:56:28 +0200

    remove create_db import

--- src/trialblazer/original_code/Descriptor_calculation/similarity_based_target_calculation.py
+++ src/trialblazer/original_code/Descriptor_calculation/similarity_based_target_calculation.py
@@ -1,9 +1,7 @@
 import numpy as np
 import pandas as pd
 from FPSim2 import FPSim2Engine
-from FPSim2.io import create_db_file
 from rdkit import Chem
-
 
 def target_features_preprocess(
     chembl_data,
@@ -89,7 +87,7 @@ def generate_h5file(preprocessed_target, outputFolder):
         "SmilesWithoutStereo"
     ].tolist()
     print(f"numbers of unique SMILES:{len(list_smi)}")
-    
+
     fpe = FPSim2Engine(outputFolder)
     print("h5 file generated!")
     return target_id_list, preprocessed_target_unique_smiles, fpe
