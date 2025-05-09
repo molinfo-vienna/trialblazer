c673460af66b79a28a51c6790f17e1ed0a23c6cb
William Schueller <william.schueller@gmail.com>
2025-05-09 20:55:02 +0200

    test

--- tests/test_draft.py
+++ tests/test_draft.py
@@ -12,7 +12,6 @@ base_model_folder = os.path.join(
 
 
 def test_draft(tmpdir):
-    # trialblazer_draft.run(out_folder=tmpdir)
     trialblazer_draft.run(
         out_folder=tmpdir, data_folder=test_data_folder, model_folder=base_model_folder
     )
